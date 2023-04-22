5c0ac716d34727c70fe20407fe869f345e92b6af
Soe Than Htut <soe48573@gmail.com>
2023-04-22 19:31:12 +0630

    'second'

--- book/models.py
+++ book/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    name=models.CharField(max_length=100)
+    name=models.CharField(max_length=100,null=False)
+    writer_name=models.CharField(max_length=200,null=True)
 
     def __str__(self):
         return self.name
